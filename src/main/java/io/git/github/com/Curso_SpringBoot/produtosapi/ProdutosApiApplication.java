39bc9f212e89ddc701907469e01d9aeacb0cce56
Eduardo Araújo <eduaraujopires370@gmail.com>
2026-01-12 16:03:52 -0300

    criado projeto

---
+++ src/main/java/io/git/github/com/Curso_SpringBoot/produtosapi/ProdutosApiApplication.java
@@ -0,0 +1,13 @@
+package io.git.github.com.Curso_SpringBoot.produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApiApplication.class, args);
+	}
+
+}
