93a25678ec8ad430491dc59d8df15dd3fd07bc71
Eduardo Araújo <eduaraujopires370@gmail.com>
2026-01-12 18:02:21 -0300

    Hellow word com Spring

--- src/main/java/io/git/github/com/Curso_SpringBoot/produtosapi/ProdutosApiApplication.java
+++ src/main/java/io/git/github/com/Curso_SpringBoot/produtosapi/ProdutosApiApplication.java
@@ -2,9 +2,17 @@ package io.git.github.com.Curso_SpringBoot.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProdutosApiApplication {
+
+	@GetMapping("/hellow")
+	public String hellowWord(){
+		return "Hellow Word";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApiApplication.class, args);
